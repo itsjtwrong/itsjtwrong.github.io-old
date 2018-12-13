b33bd46c2c29cf58c695d918f363156143f79b69
JT Wright <jtwright@knox.edu>
2018-12-12 17:12:15 -0700

    form validation finished

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -8,16 +8,71 @@
     </div>
   <h2 v-if="cartItems.length > 0" class="checkout" @click="showForm = true">Checkout | Total ${{addPrice()}} (ships to US)</h2>
   <h2 v-else>No items in cart</h2>
-  <form v-if="showForm" action="">
-    First Name: <input type="text" name="firstname"><br>
-    Last Name: <input type="text" name="lastname"><br>
-    Email: <input type="email" name="email" id=""><br>
+  <form v-if="showForm" action="" class='checkoutForm'>
+    Full Name * <br><input required placeholder="First" type="text" name="firstname"><br>
+    <input required placeholder="Last" type="text" name="lastname"><br>
+    Email: *<input required v-validate="'required|email'" type="email" name="email" id=""><br>
     Phone: <input type="tel" name="phone" id=""><br>
-    Street Address: <input type="text" name="street" id=""><br>
+    Street Address: *<input required type="text" name="street" id=""><br>
     Street Address Line 2: <input type="text" name="line2" id=""><br>
-    City: <input type="text" name="city" id=""><br>
-    State: <input type="text" name="state" id=""><br>
-    Zip Code: <input type="text" name="zip" id=""><br>
+    City: *<input required type="text" name="city" id=""><br>
+    State: *<select>
+	<option value="AL">Alabama</option>
+	<option value="AK">Alaska</option>
+	<option value="AZ">Arizona</option>
+	<option value="AR">Arkansas</option>
+	<option value="CA">California</option>
+	<option value="CO">Colorado</option>
+	<option value="CT">Connecticut</option>
+	<option value="DE">Delaware</option>
+	<option value="DC">District Of Columbia</option>
+	<option value="FL">Florida</option>
+	<option value="GA">Georgia</option>
+	<option value="HI">Hawaii</option>
+	<option value="ID">Idaho</option>
+	<option value="IL">Illinois</option>
+	<option value="IN">Indiana</option>
+	<option value="IA">Iowa</option>
+	<option value="KS">Kansas</option>
+	<option value="KY">Kentucky</option>
+	<option value="LA">Louisiana</option>
+	<option value="ME">Maine</option>
+	<option value="MD">Maryland</option>
+	<option value="MA">Massachusetts</option>
+	<option value="MI">Michigan</option>
+	<option value="MN">Minnesota</option>
+	<option value="MS">Mississippi</option>
+	<option value="MO">Missouri</option>
+	<option value="MT">Montana</option>
+	<option value="NE">Nebraska</option>
+	<option value="NV">Nevada</option>
+	<option value="NH">New Hampshire</option>
+	<option value="NJ">New Jersey</option>
+	<option value="NM">New Mexico</option>
+	<option value="NY">New York</option>
+	<option value="NC">North Carolina</option>
+	<option value="ND">North Dakota</option>
+	<option value="OH">Ohio</option>
+	<option value="OK">Oklahoma</option>
+	<option value="OR">Oregon</option>
+	<option value="PA">Pennsylvania</option>
+	<option value="RI">Rhode Island</option>
+	<option value="SC">South Carolina</option>
+	<option value="SD">South Dakota</option>
+	<option value="TN">Tennessee</option>
+	<option value="TX">Texas</option>
+	<option value="UT">Utah</option>
+	<option value="VT">Vermont</option>
+	<option value="VA">Virginia</option>
+	<option value="WA">Washington</option>
+	<option value="WV">West Virginia</option>
+	<option value="WI">Wisconsin</option>
+	<option value="WY">Wyoming</option>
+</select>				
+		<br>
+    Zip Code: *<input required type="text" name="zip" id=""><br>
+    <button type="submit">Next</button>
+    (* = required)
   </form>
   </div>
 </template>
@@ -46,9 +101,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+  input, button {
+    border: 1px solid #111;
+    margin:2px;
+  }
+
   .cart {
     margin: 10px;
   }
+
+  .checkoutForm {
+    border: 1px solid black;
+    padding:10px;
+  }
+
   .checkout {
     background:black;
     color:white;
